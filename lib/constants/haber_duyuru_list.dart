8f630521077e2ed10d747f1d69d111912c72d59e
Umutcan <73028201+softwareTR-Can@users.noreply.github.com>
2023-04-08 00:40:48 +0300

    haber ve duyuru list

---
+++ lib/constants/haber_duyuru_list.dart
@@ -0,0 +1,45 @@
+class Haber {
+  Haber(this.imageUrl, this.baslik, this.aciklama);
+
+  String imageUrl;
+  String baslik;
+  String aciklama;
+}
+
+List haberList = [
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Google Oyun ve Uygulama Akademisi 2.yılında 35.767 başvuru aldı! Oyun ve Uygulama Akademisi’ne olan ilginiz için çok teşekkür ederiz.",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+  Haber(
+    "https://scontent.cdninstagram.com/v/t51.2885-15/313551104_477995174144848_1282073804536158579_n.jpg?stp=dst-jpg_e15&_nc_ht=scontent.cdninstagram.com&_nc_cat=104&_nc_ohc=IVWSn5Am6YYAX9cqOKx&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=Mjk2MTk1OTc0MDQ5NDMwNDAyNw%3D%3D.2-ccb7-5&oh=00_AfA1V2RW1WCQMFvnQRUq4l96rg_2jGd_WtHLu-2fEZWvPg&oe=643529FE&_nc_sid=978cb9",
+    "Başlık",
+    "Açıklama",
+  ),
+];
