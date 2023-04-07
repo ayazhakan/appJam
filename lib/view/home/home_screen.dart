6a321476ff2fa0b74053d437717a9f80ffff193f
Umutcan <73028201+softwareTR-Can@users.noreply.github.com>
2023-04-07 22:21:58 +0300

    anasayfa

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,16 +11,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double startDegreeOffset = 180;
+
   @override
   Widget build(BuildContext context) {
+    startDegreeOffsetTimer();
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("Anasayfa"),
       ),
       body: ListView(
-        ///todo: Eğitimlerin tamamlanma yüzdesi card -- grafikler -- eğitimler arası geçiş
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  "Yuvarlak Grafik",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
 
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void startDegreeOffsetTimer() {
+    Timer.periodic(Duration(milliseconds: 10000), (_) {
+      setState(() {
+        startDegreeOffset++;
+      });
+    });
+  }
+
+
+  AspectRatio buildGraph() {
+    return AspectRatio(
+                aspectRatio: 1,
+                child: PieChart(
+                  PieChartData(
+                    sections: [
+                      PieChartSectionData(
+                        value: 25,
+                        title: '25%',
+                        color: Colors.green,
+                      ),
+                      PieChartSectionData(
+                        value: 75,
+                        title: '75%',
+                        color: Colors.red,
+                      ),
+                    ],
+                    sectionsSpace: 2,
+                    centerSpaceRadius: 40,
+                    startDegreeOffset: startDegreeOffset,
+                  ),
+                ),
+              );
   }
 }
