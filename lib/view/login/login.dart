8b6c3ebbdab70cd5cb43b397b5ea9599734014a6
Hakan Ayaz <hakanayaz9844@gmail.com>
2023-04-09 21:38:06 +0300

    loginPageFixed

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -15,11 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   get passwordController => null;
   final _text = TextEditingController();
   final _text2 = TextEditingController();
-
-   bool _validate = false;
-
   bool _validate = false;
-
 
   @override
   void dispose() {
@@ -31,14 +27,13 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        /*
+      /*
         appBar: AppBar(
           title: Text(""),
           backgroundColor: yesil,
         ),*/
         body: ListView(children: [
-
-            SizedBox(height: 25),
+          SizedBox(height: 25),
 
           Container(
             height: 120,
@@ -47,69 +42,6 @@ class _LoginPageState extends State<LoginPage> {
           Container(
               alignment: Alignment.center,
               padding: const EdgeInsets.all(10),
-
-      SizedBox(height: 65),
-      Container(
-        height: 120,
-        child: Image.asset("assets/sign-up.png"),
-      ),
-      Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(10),
-          child: const Text(
-            'Google Oyun ve Uygulama Akademisi',
-            style: TextStyle(
-                color: Color(0xff418cf5),
-                fontSize: 30,
-                fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          )),
-      SizedBox(height: 25),
-      Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(10),
-          child: const Text('Giriş Yap',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ))),
-      Container(
-        padding: const EdgeInsets.all(10),
-        child: TextField(
-          controller: _text,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'E posta',
-            errorText: _validate ? 'E-posta boş olamaz' : null,
-          ),
-        ),
-      ),
-      Container(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: TextField(
-          controller: _text2,
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Şifre',
-            suffixText: '*',
-            errorText: _validate ? 'E-posta boş olamaz' : null,
-          ),
-        ),
-      ),
-      SizedBox(
-        height: 20,
-      ),
-      Container(
-          height: 50,
-          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-          child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                    yesil), // Here Im having the error
-              ),
-
               child: const Text(
                 'Google Oyun ve Uygulama Akademisi',
                 style: TextStyle(
@@ -131,13 +63,12 @@ class _LoginPageState extends State<LoginPage> {
           Container(
             padding: const EdgeInsets.all(10),
             child: TextField(
-                controller: _text,
+              controller: _text,
               decoration:  InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'E posta',
                 errorText: _validate ? 'E-posta boş olamaz' : null,
               ),
-
             ),
           ),
           Container(
@@ -160,13 +91,13 @@ class _LoginPageState extends State<LoginPage> {
               height: 50,
               padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      yesil), // Here Im having the error
-                ),
-                child: const Text(
-                  'Giriş',
-                ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        yesil), // Here Im having the error
+                  ),
+                  child: const Text(
+                    'Giriş',
+                  ),
                   onPressed: () {
                     setState(() {
                       _text.text.isEmpty ? _validate = true : _validate = false;
@@ -174,8 +105,8 @@ class _LoginPageState extends State<LoginPage> {
                     });
 
                     if(_text.text.isEmpty!=true && _text2.text.isEmpty!=true){
-                    Get.offAll(BottomNavBarPage());
-                  }}
+                      Get.offAll(BottomNavBarPage());
+                    }}
 
 
               )),
@@ -196,36 +127,8 @@ class _LoginPageState extends State<LoginPage> {
           ),
 
 
-    ]
-    )
+        ]
+        )
     );
-
-              onPressed: () {
-                setState(() {
-                  _text.text.isEmpty ? _validate = true : _validate = false;
-                  _text2.text.isEmpty ? _validate = true : _validate = false;
-                });
-
-                if (_text.text.isEmpty != true && _text2.text.isEmpty != true) {
-                  Get.offAll(BottomNavBarPage());
-                }
-              })),
-      Row(
-        children: <Widget>[
-          const Text('Hesabın yok mu?'),
-          TextButton(
-            child: const Text(
-              'Kaydol',
-              style: TextStyle(fontSize: 20),
-            ),
-            onPressed: () {
-              //signup screen
-            },
-          )
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
-      ),
-    ]));
-
   }
 }
