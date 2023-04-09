f20cfd225a1397ca8e03f941037cc8d39ec16e4a
Umutcan <73028201+softwareTR-Can@users.noreply.github.com>
2023-04-09 14:31:27 +0300

    Update login.dart

--- lib/view/login/login.dart
+++ lib/view/login/login.dart
@@ -17,101 +17,97 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        /*
         appBar: AppBar(
           title: Text(""),
           backgroundColor: yesil,
-        ),
+        ),*/
         body: ListView(
-          children: [
-            SizedBox(height: 25),
-            Container(
-              height: 120,
-              child: Image.asset("assets/sign-up.png"),
+      children: [
+        SizedBox(height: 65),
+        Container(
+          height: 120,
+          child: Image.asset("assets/sign-up.png"),
+        ),
+        Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Google Oyun ve Uygulama Akademisi',
+              style: TextStyle(
+                  color: Color(0xff418cf5),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            )),
+        SizedBox(height: 25),
+        Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text('Giriş Yap',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ))),
+        Container(
+          padding: const EdgeInsets.all(10),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'E posta',
             ),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Google Oyun ve Uygulama Akademisi',
-                  style: TextStyle(
-                      color: Color(0xff418cf5),
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                )),
-            SizedBox(height: 25),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text('Giriş Yap',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ))),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'E posta',
-                ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+          child: TextField(
+            obscureText: true,
+            controller: passwordController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Şifre',
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    yesil), // Here Im having the error
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Şifre',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
               child: const Text(
-                'Şifremi Unuttum',
+                'Giriş',
               ),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(
-                        yesil), // Here Im having the error
-                  ),
-                  child: const Text(
-                    'Giriş',
-                  ),
-                  onPressed: () {
-                    var nameController;
-                    var passwordController;
-                    //print(nameController.text);
-                    //print(passwordController.text);
-                    Get.offAll(BottomNavBarPage());
-                  },
-                )),
-            Row(
-              children: <Widget>[
-                const Text('Hesabın yok mu?'),
-                TextButton(
-                  child: const Text(
-                    'Kaydol',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
+              onPressed: () {
+                var nameController;
+                var passwordController;
+                //print(nameController.text);
+                //print(passwordController.text);
+                Get.offAll(BottomNavBarPage());
+              },
+            )),
+        Row(
+          children: <Widget>[
+            const Text('Hesabın yok mu?'),
+            TextButton(
+              child: const Text(
+                'Kaydol',
+                style: TextStyle(fontSize: 20),
+              ),
+              onPressed: () {
+                //signup screen
+              },
+            )
           ],
-        ));
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+      ],
+    ));
   }
 }
